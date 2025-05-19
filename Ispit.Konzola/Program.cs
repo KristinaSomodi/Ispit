627abd259f879ae7566f3a4f82fce313f05e62e2
KristinaSomodi <84789456+KristinaSomodi@users.noreply.github.com>
2025-05-19 20:31:28 +0200

    Main - Ispit

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,54 @@
+﻿using Ispit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucenik ucenik1 = new Ucenik()
+            {
+                Ime = "Luka",
+                Prezime = "Lukic",
+                DatumRodjenja = new DateTime(2000, 5, 23),
+                Prosjek = 1.55
+            };
+            Ucenik ucenik2 = new Ucenik()
+            {
+                Ime = "Mia",
+                Prezime = "Mijic",
+                DatumRodjenja = new DateTime(1998, 2, 15),
+                Prosjek = 3.22
+            };
+            Ucenik ucenik3 = new Ucenik()
+            {
+                Ime = "Luka",
+                Prezime = "Mijic",
+                DatumRodjenja = new DateTime(1995, 9, 4),
+                Prosjek = 4.33
+            };
+
+            Console.WriteLine($"Ime: {ucenik1.Ime}");
+            Console.WriteLine($"Prezime: {ucenik1.Prezime}");
+            Console.WriteLine($"Datum rodjenja: {ucenik1.DatumRodjenja}");
+            Console.WriteLine($"Prosjek: {ucenik1.Prosjek}");
+
+            Console.WriteLine($"Ime: {ucenik2.Ime}");
+            Console.WriteLine($"Prezime: {ucenik2.Prezime}");
+            Console.WriteLine($"Datum rodjenja: {ucenik2.DatumRodjenja}");
+            Console.WriteLine($"Prosjek: {ucenik2.Prosjek}");
+
+            Console.WriteLine($"Ime: {ucenik3.Ime}");
+            Console.WriteLine($"Prezime: {ucenik3.Prezime}");
+            Console.WriteLine($"Datum rodjenja: {ucenik3.DatumRodjenja}");
+            Console.WriteLine($"Prosjek: {ucenik3.Prosjek}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public double Prosjek { get; set; }
+
+
+        public int Starost(DateTime datumRodjenja)
+        {
+            DateTime danas = DateTime.Today;
+            int starost = danas.Year - datumRodjenja.Year;
+            return starost;
+        }
+
+        public void IspisProsjeka(double prosjek)
+        {
+            if(prosjek >= 1 && prosjek <= 1.49)
+            {
+                Console.WriteLine("nedovoljan");
+            } 
+            else if (prosjek >= 1.50 && prosjek <= 2.49)
+            {
+                Console.WriteLine("dovoljan");
+            }
+            else if (prosjek >= 2.50 && prosjek <= 3.49)
+            {
+                Console.WriteLine("dobar");
+            }
+            else if (prosjek >= 3.50 && prosjek <= 4.49)
+            {
+                Console.WriteLine("vrlo dobar");
+            }
+            else if (prosjek >= 4.50 && prosjek <= 5)
+            {
+                Console.WriteLine("odlican");
+            }
+        }
+    }
+}
